614fed3864f81e1fa87ad76e518b3177f2aac598
Willie <990988169@qq.com>
2026-06-02 13:39:36 +0800

    init

--- start.sh
+++ start.sh
@@ -3,5 +3,10 @@ mkdir -p /config
 cat > /config/configuration.yaml <<EOF
 http:
   server_port: ${PORT:-8123}
+  use_x_forwarded_for: true
+  trusted_proxies:
+    - ::1
+    - 127.0.0.1
+    - 10.0.0.0/8
 EOF
 exec python3 -m homeassistant --config /config
